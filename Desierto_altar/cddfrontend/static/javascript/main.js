41b6d9e263fa4615bd0b57246bff82c8a404c039
ErikFantomex <a217213318@unison.mx>
2024-01-28 22:49:08 -0700

    feats: to or more funciotns add_map

--- Desierto_altar/cddfrontend/static/javascript/main.js
+++ Desierto_altar/cddfrontend/static/javascript/main.js
@@ -5,6 +5,22 @@ function init(){
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map)
+
+    const fetchGetRequest = async(url,func)=>{
+      try {
+        const response = await fetch(url)
+        const json = await response.json()
+        return func(json)
+        
+      } catch (error) {
+        console.log(error)
+        
+      }
+    }
+    const addAllPlacesToMap=(json)=>{
+      console.log(json)
+    }
+    fetchGetRequest('/api/v1/places',addAllPlacesToMap)
   
 }
 //29.1026, -110.97732.
