0f4c64e0215a25865200ec80cfa9296bc1fe6980
ErikFantomex <a217213318@unison.mx>
2024-01-28 22:16:13 -0700

    feats:add leaflet

--- Desierto_altar/cddfrontend/static/javascript/main.js
+++ Desierto_altar/cddfrontend/static/javascript/main.js
@@ -1,1 +1,10 @@
-window.alert('Hello from main.js')
+document.addEventListener('DOMContentLoaded',init)
+
+function init(){
+    const map = L.map('map').setView([29.10, -110.97], 4)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map)
+  
+}
+//29.1026, -110.97732.
